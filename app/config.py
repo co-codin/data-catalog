7b825d8dc01fec2880697eee0d62663f72f2d790
achekanov <artur.chekanov@itentika.ru>
2022-11-30 23:40:05 +0300

    prod images and compose fixes

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     debug: bool = False
     log_dir: str = "/var/log/n3dwh/"
     log_name: str = "data_catalog.log"
-    neo4j_connection_string: str = 'bolt://graph_db:7687'
+    neo4j_connection_string: str = 'bolt://graphdb.lan:7687'
 
 
 settings = Settings()
