f781d21ad1fb47141bcbca579b7e0137d213e3d8
vladimir kondratenko <vladimir.kondratenko@itentika.ru>
2022-11-30 13:20:05 +0300

    Left only necessary entries in requrements.txt

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    NEO4J_CONNECTION_STRING: str = 'bolt://graph_db:7687'
+    NEO4J_CONNECTION_STRING: str = 'bolt://localhost:7687'
 
 
 settings = Settings()
