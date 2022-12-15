fdd29ca13f73fc743ce28e61a0f90f34165a08ec
achekanov <achekanov@itentika.ru>
2022-12-15 18:20:35 +0300

    neo4j creds added

--- app/config.py
+++ app/config.py
@@ -1,4 +1,8 @@
+from collections import namedtuple
 from pydantic import BaseSettings
+
+
+Neo4jCreds = namedtuple('Neo4jCreds', ['username', 'password'])
 
 
 class Settings(BaseSettings):
@@ -6,6 +10,7 @@ class Settings(BaseSettings):
     log_dir: str = "/var/log/n3dwh/"
     log_name: str = "data_catalog.log"
     neo4j_connection_string: str = 'bolt://graphdb.lan:7687'
+    neo4j_auth: Neo4jCreds = ('neo4j', 'dwh')
 
     class Config:
         env_prefix = "dwh_data_catalog_"
