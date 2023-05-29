703d2d5dc443f7c75d9079dac16e1bb187cf31f0
elijah <cuiyeqing960904@gmail.com>
2023-05-29 05:28:00 -0400

    model id removed while updating model version

--- app/schemas/model_version.py
+++ app/schemas/model_version.py
@@ -14,7 +14,6 @@ class ModelVersionIn(BaseModel):
 
 
 class ModelVersionUpdateIn(BaseModel):
-    model_id: Optional[int] = None
     status: str = Field(None, max_length=100)
     owner: Optional[str] = Field(None, max_length=36*4)
     desc: Optional[str] = Field(None, max_length=500)
