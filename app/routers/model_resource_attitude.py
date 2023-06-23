c979087bf948bb43a9ce9b2bff9a45963e6ac09a
abagyshov <alexander.bagyshov@itentika.ru>
2023-06-23 16:29:39 +0300

    N3BI20-282 attitude attributes

--- app/routers/model_resource_attitude.py
+++ app/routers/model_resource_attitude.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Depends
 from app.dependencies import db_session, get_user, get_token
-from app.crud.crud_model_attitude import read_attitudes_by_resource_id, read_attitude_by_guid, create_model_attitude, \
-    delete_model_attitude
+from app.crud.crud_model_attitude import read_attitudes_by_resource_id, create_model_attitude, delete_model_attitude
 from app.schemas.model_attitude import ModelAttitudeIn
 
 router = APIRouter(
@@ -13,11 +12,6 @@ router = APIRouter(
 @router.get('/by_resource/{resource_id}')
 async def get_model_attitudes(resource_id: int, session=Depends(db_session), user=Depends(get_user)):
     return await read_attitudes_by_resource_id(resource_id, session)
-
-
-@router.get('/{guid}')
-async def get_model_attitude(guid: str, session=Depends(db_session), token=Depends(get_token)):
-    return await read_attitude_by_guid(guid, token, session)
 
 
 @router.post('/')
