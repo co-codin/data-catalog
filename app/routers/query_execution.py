a730efb50de44b611b84b538db47e1930d7a3627
elijah <cuiyeqing960904@gmail.com>
2023-07-19 09:47:37 -0400

    demo built

--- app/routers/query_execution.py
+++ app/routers/query_execution.py
@@ -1,9 +1,24 @@
-import asyncio
-
 from fastapi import APIRouter, Depends
-
+from typing import Dict
+from app.models.queries import QueryExecution
+from app.dependencies import db_session, get_user
+from sqlalchemy import update
 
 router = APIRouter(
     prefix='/query_executions',
     tags=['query executions']
 )
+
+
+@router.put('/{guid}/publish', response_model=Dict[str, str])
+async def publish_query_execution(
+        guid: str,  publish_name: str, session=Depends(db_session), _=Depends(get_user)
+):
+   await session.execute(
+        update(QueryExecution)
+        .where(QueryExecution.guid == guid)
+        .values(
+            publish_name=publish_name
+        )
+    )
+
