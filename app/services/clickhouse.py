431af6f296c4f8c902b5383a4d5a7a146c69e29d
abagyshov <alexander.bagyshov@itentika.ru>
2023-07-20 10:51:59 +0300

    clickhouse

--- app/services/clickhouse.py
+++ app/services/clickhouse.py
@@ -1,21 +1,16 @@
 import logging
-import psycopg
-import asyncio
 import clickhouse_connect
 
-from clickhouse_connect.driver.exceptions import DatabaseError
-
-from executor_service.errors import QueryNotRunning
 from app.config import settings
 
-
 LOG = logging.getLogger(__name__)
+
 
 class ClickhouseService:
     def __init__(self):
         self._conn_string = settings.clickhouse_connection_string
         self.client = None
-    
+
     def connect(self):
         self.client = clickhouse_connect.get_client(dsn=settings.clickhouse_connection_string)
 
@@ -41,7 +36,7 @@ class ClickhouseService:
         return self.client.query(
             "SELECT * FROM publish WHERE publish_name = '{}'".format(publish_name)
         )
-    
+
     def update(self, query_id, dest_type, published_at, publish_name, publish_status, status, finished_at):
         self.client.update(
             "publish",
