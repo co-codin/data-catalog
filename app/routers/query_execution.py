d54c104dad298e57177867624c561dc8a8d27bae
abagyshov <alexander.bagyshov@itentika.ru>
2023-07-20 10:47:44 +0300

    clickhouse

--- app/routers/query_execution.py
+++ app/routers/query_execution.py
@@ -2,11 +2,9 @@ from fastapi import APIRouter, Depends
 from typing import Dict
 from app.models.queries import QueryExecution
 from app.dependencies import db_session, get_user
-from sqlalchemy import update
-from app.services.clickhouse_service import ClickhouseService
+from sqlalchemy import update, select
 
-
-
+from app.services.clickhouse import ClickhouseService
 
 router = APIRouter(
     prefix='/query_executions',
@@ -16,25 +14,58 @@ router = APIRouter(
 
 @router.put('/{guid}/publish', response_model=Dict[str, str])
 async def publish_query_execution(
-        guid: str,  publish_name: str, session=Depends(db_session), _=Depends(get_user)
+        guid: str, publish_name: str, publish_status: str, force: bool = True, session=Depends(db_session), _=Depends(get_user)
 ):
-   '''
-    должна быть опция "перезатирать одноименную публикацию". 
-    Если опция активна, то если в хранилище есть публикация с точно таким же именем,
-    то мы меняем текущую публикацию с таким же именем на текущий результат.
-     Если публикация неактивна, но в хранилище есть публикация с таким же именем,
- то мы должны предупредить пользователя об этом и попросить изменить имя текущей публикации.
-     По умолчанию опция активна. '''
-   
-#    clickhouseService = ClickhouseService
-#    clickhouseService.connect()
-#    clickhouseService.createTable()
-
-
-   await session.execute(
-        update(QueryExecution)
+    query_execution = await session.execute(
+        select(QueryExecution)
         .where(QueryExecution.guid == guid)
-        .values(
-            publish_name=publish_name
-        )
     )
+    query_execution = query_execution.scalars().first()
+
+    clickhouseService = ClickhouseService()
+    clickhouseService.connect()
+    clickhouseService.createPublishTable()
+    search_result = clickhouseService.getByName(publish_name)
+
+    if force:
+        if len(search_result.result_set):
+            clickhouseService.update(
+                query_id=query_execution.query_id,
+                dest_type=query_execution.desc_type,
+                published_at=query_execution.publiches_at,
+                publish_name=publish_name,
+                publish_status=publish_status,
+                status=publish_status,
+                finished_at=query_execution.finished_at
+            )
+
+            await session.execute(
+                update(QueryExecution)
+                .where(QueryExecution.guid == guid)
+                .values(
+                    publish_name=publish_name,
+                )
+            )
+        else:
+            clickhouseService.insert(
+                query_id=query_execution.query_id,
+                dest_type=query_execution.dest_type,
+                published_at=query_execution.published_at,
+                publish_name=publish_name,
+                publish_status=publish_status,
+                status=publish_status,
+                finished_at=query_execution.finished_at
+            )
+    else:
+        if len(search_result.result_set):
+            return 'Publish name exists. Try change it'
+        else:
+            clickhouseService.insert(
+                query_id=query_execution.query_id,
+                dest_type=query_execution.dest_type,
+                published_at=query_execution.published_at,
+                publish_name=publish_name,
+                publish_status=publish_status,
+                status=publish_status,
+                finished_at=query_execution.finished_at
+            )
